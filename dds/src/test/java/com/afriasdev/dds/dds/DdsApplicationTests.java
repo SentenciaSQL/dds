cd30e24002ae4a498b069ee2735e11babcb4d53d
Andrés Frías <inf512.andres.frias@gmail.com>
2025-07-18 17:53:16 -0400

    primer commit

---
+++ dds/src/test/java/com/afriasdev/dds/dds/DdsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.afriasdev.dds.dds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DdsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
